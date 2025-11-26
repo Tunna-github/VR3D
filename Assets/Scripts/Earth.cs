007d97f3e034e706fe9aaaccf81489be7f543a23
Kuni <binlen11112@gmail.com>
2025-11-26 10:05:58 +0700

    change update in Earth script to fixedUpdate for no more gitchy

--- Assets/Scripts/Earth.cs
+++ Assets/Scripts/Earth.cs
@@ -21,7 +21,7 @@ public class Earth : MonoBehaviour
         transform.rotation = Quaternion.Euler(23.5f, 0f, 0f);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (sunTransform == null)
         {
